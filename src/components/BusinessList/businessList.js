f5f25bcd800770e3945ad12108ee35d7172e7381
Tim Albert <talbert1209@gmail.com>
2018-02-27 17:58:37 -0600

    linked to Yelp API and finished project

--- src/components/BusinessList/businessList.js
+++ src/components/BusinessList/businessList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './businessList.css';
+
 import Business from '../Business/business';
 
 class BusinessList extends React.Component {
@@ -8,11 +9,12 @@ class BusinessList extends React.Component {
       <div className="BusinessList">
         {
           this.props.businesses.map(business => {
-            return <Business business={business} />;
+            return <Business business={business}
+            key={business.id} />
           })
         }
       </div>
-    )
+    );
   }
 }
 
